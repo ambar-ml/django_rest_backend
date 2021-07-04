44a2ee081209d120cfe5f44790bec2016f205306
ambar-ml <68280445+ambar-ml@users.noreply.github.com>
2021-07-05 02:52:52 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -18,7 +18,7 @@ import dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().root.root
+BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
