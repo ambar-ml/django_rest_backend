ed39e5e5438d44c4dc888632b818f235c6a574cd
Ambar <ambar007gupta@gmail.com>
2021-07-06 12:33:12 +0530

    localhost

--- ecom/settings.py
+++ ecom/settings.py
@@ -160,7 +160,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     'https://django-rest-backend-ecom.herokuapp.com',
-    'https://react-frontend-ecom.herokuapp.com'
+    'https://react-frontend-ecom.herokuapp.com',
+    'http://localhost:3000/'
 ]
 
 
